21345a1778b8034e0a75267dc8dbd39841b21f39
hanl <hanl@ids-mannheim.de>
2013-10-27 21:16:45 +0100

    added parser script

---
+++ artifacts/parser.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+## set runtime parameters 
+## in "all" mode the rule file is automatically detected, if there are rule files in the "config"
+## directory with the respective language extension ("EN" or "DE").
+v3="dkpro.ukp.topic.structure.jar"
+input="input"
+output="output"
+modal="pcfg"
+
+parameters="-parser $1 -modal $modal -inDir $input -outDir $output"
+
+java -jar $v3 $parameters
+
+
+## java -cp annolab.jar org.annolab.tre.cli.Main Rules_EN.xml file.xml
+## java -cp annolab.jar org.annolab.tre.cli.LearnMain Rules_EN.xml file.xml out.xml
